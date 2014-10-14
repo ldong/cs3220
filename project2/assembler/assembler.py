1a8887be249e43538dbf9363a33e4df64c66fc96
Lin Dong <ldong@gatech.edu>
2014-10-14 13:44:18 -0400

    Finally found what each register is for from MIPS

--- project2/assembler/assembler.py
+++ project2/assembler/assembler.py
@@ -11,38 +11,38 @@ header = {
 
 # 32 registers from r0 to r31
 registers = {
-        'r0' :      '00000',
-        'r1' :      '00001',
-        'r2' :      '00010',
-        'r3' :      '00011',
-        'r4' :      '00100',
-        'r5' :      '00101',
-        'r6' :      '00110',
-        'r7' :      '00111',
-        'r8' :      '01000',
-        'r9' :      '01001',
-        'r10':      '01010',
-        'r11':      '01011',
-        'r12':      '01100',
-        'r13':      '01101',
-        'r14':      '01110',
-        'r15':      '01111',
-        'r16':      '10000',
-        'r17':      '10001',
-        'r18':      '10010',
-        'r19':      '10011',
-        'r20':      '10100',
-        'r21':      '10101',
-        'r22':      '10110',
-        'r23':      '10111',
-        'r24':      '11000',
-        'r25':      '11001',
-        'r26':      '11010',
-        'r27':      '11011',
-        'r28':      '11100',
-        'r29':      '11101',
-        'r30':      '11110',
-        'r31':      '11111'
+        'zero' :      '00000', # r0'
+        'at'   :      '00001', # r1'
+        'v0'   :      '00010', # r2'
+        'v1'   :      '00011', # r3'
+        'a0'   :      '00100', # r4'
+        'a1'   :      '00101', # r5'
+        'a2'   :      '00110', # r6'
+        'a3'   :      '00111', # r7'
+        't0'   :      '01000', # r8'
+        't1'   :      '01001', # r9'
+        't2'   :      '01010', # r10
+        't3'   :      '01011', # r11
+        't4'   :      '01100', # r12
+        't5'   :      '01101', # r13
+        't6'   :      '01110', # r14
+        't7'   :      '01111', # r15
+        's0'   :      '10000', # r16
+        's1'   :      '10001', # r17
+        's2'   :      '10010', # r18
+        's3'   :      '10011', # r19
+        's4'   :      '10100', # r20
+        's5'   :      '10101', # r21
+        's6'   :      '10110', # r22
+        's7'   :      '10111', # r23
+        't8'   :      '11000', # r24
+        't9'   :      '11001', # r25
+        'k0'   :      '11010', # r26
+        'k1'   :      '11011', # r27
+        'gp'   :      '11100', # r28
+        'sp'   :      '11101', # r29
+        'fp'   :      '11110', # r30
+        'ra'   :      '11111'  # r31
         }
 
 # Instruction Set Architecture from project-isa.pdf file in binaries
@@ -121,9 +121,36 @@ opcodes = {
         'JMP': '',
         }
 
+
 instruction_line_number = 0
 names = {}
 origs = []
+
+# look up instruciton type
+isa_type_12_zero = set([
+                'ADD', 'SUB', 'AND', 'OR', 'XOR', 'NAND', 'NOR', 'XNOR',
+                'F', 'EQ', 'LT', 'LTE', 'T', 'NE', 'GTE', 'GT'])
+isa_type_imm = set([
+    'ADDI', 'SUBI', 'ANDI', 'ORI', 'XORI', 'NANDI', 'NORI', 'XNORI',
+    'LW', 'SW',
+    'FI', 'EQI', 'LTI', 'LTEI', 'TI', 'NEI', 'GTEI', 'GTI'])
+isa_type_mvhi = set(['MVHI'])
+isa_type_jal = set(['JAL'])
+isa_type_pcrel = set([
+    'BF', 'BEQ', 'BLT', 'BLTE', 'BEQZ', 'BLTZ', 'BLTEZ'
+    ])
+isa_type_implemented = {
+        'BR':   'BEQ',
+        'SUBI': 'ADDI',
+        'NOT':  'NAND',
+        'BLT':  'LT',
+        'BLE':  'LE',
+        'BGT':  'GT',
+        'BGE':  'GE',
+        'CALL': 'JAL',
+        'RET':  'JAL',
+        'JMP':  'JAL'
+        }
 
 def main():
     fname = 'Test.a32'
@@ -149,28 +176,36 @@ def read_file(fname):
     print 'Reading file: {}'.format(fname)
     with open (fname, 'r') as f:
         file_line_number = 1
+        total_instructions = 0
         for line in f.xreadlines():
             print file_line_number,
             if re.match(r'^\s*$', line):
-                print 'Line is empty'
+                # print 'Line is empty'
                 pass
             if re.match(r'^\s*;', line):
-                print 'Line is a comment'
+                # print 'Line is a comment'
                 pass
             if re.match(r'\s*.ORIG', line):
                 print 'Line has orig'
                 curr = line.split()
                 origs.append(curr[1])
-                pass
             if re.match(r'\s([a-zA-Z]+)', line):
                 print 'Line has instruction'
-                pass
+                curr = line.split()
+                instruction = curr[0]
+                curr = ''.join([e for i, e in enumerate(line.split()) if i > 0])
+                curr = curr.split(',')
+                rd = curr[0]
+                rs1 = curr[1]
+                rs2 = curr[2]
+                import ipdb; ipdb.set_trace()
+                total_instructions += 1
             if re.match(r'[a-zA-Z0-9]+:', line):
                 print 'Line has section label'
                 pass
             if re.match(r'\s*.NAME', line):
                 print 'Line has NAME variables'
-                curr = ''.join([e for i, e in enumerate(line.split()) if i > 0 ])
+                curr = ''.join([e for i, e in enumerate(line.split()) if i > 0])
                 curr = curr.split('=')
                 names[curr[0]] = curr[1]
                 pass
@@ -179,6 +214,65 @@ def read_file(fname):
     print origs
     import ipdb; ipdb.set_trace()
 
+
+def convert_instruction_to_binary(instruction, rd, rs1, rs2, imm):
+    ''' convert instruction into binary'''
+    binary = 0
+    inst_binary = opcodes.get(rd)
+    return binary
+
+def convert_instruction_to_correct_instruction(instruction, rd, rs1, rest):
+    ''' return a tuble of instruction binarys '''
+    first_instruction = 0
+    second_instruction= 0
+
+    if instruction.upper() in isa_type_implemented:
+        temp_instruction = isa_type_implemented.get(instruction)
+        if temp_instruction == 'BEQ':
+            pass
+        elif temp_instruction == 'ADDI':
+            pass
+        elif temp_instruction == 'NAND':
+            pass
+        elif temp_instruction == 'LT':
+            pass
+        elif temp_instruction == 'LE':
+            pass
+        elif temp_instruction == 'GT':
+            pass
+        elif temp_instruction == 'GE':
+            pass
+        # convert one instruction into two instructions
+        elif temp_instruction == 'JAL':
+            if instruction == 'CALL':
+                pass
+            elif instruction == 'RET':
+                pass
+            elif instruction == 'JMP':
+                pass
+    elif instruction.upper() in isa_type_12_zero:
+        # 12 bits
+        zeros = '000000000000'
+        r2 = rest
+        first_instruction = convert_instruction_to_binary(instruction, rd, rs1, r2, zeros)
+    elif instruction.upper() in isa_type_imm:
+        imm = None
+        first_instruction = convert_instruction_to_binary(instruction, rd, rs1, r2, imm)
+    elif instruction.upper() in isa_type_mvhi:
+        immHi = None
+        first_instruction = convert_instruction_to_binary(instruction, rd, '0000', None, immHi)
+    elif instruction.upper() in isa_type_jal:
+        # 16 bits
+        shImm = None
+        first_instruction = convert_instruction_to_binary(instruction, rd, rs1, None, shImm)
+    elif instruction.upper() in isa_type_pcrel:
+        # 16 bits
+        pcrel = None
+        first_instruction = convert_instruction_to_binary(instruction, None, rs1, rs2, imm)
+    else:
+        print 'Instruction {} not found, exiting program'.format(instruction)
+        sys.exit(1)
+    return (first_instruction, second_instruction)
 
 def parse_file(fname):
     pass
@@ -221,8 +315,11 @@ def write_footer(file):
 def write_change_line(file):
     file.write('\n')
 
-def bin2hex(b_str, prefix):
+def bin2hex(b_str):
     ''' return binary string to hex format'''
+    if(len(b_str) != 32):
+        print 'its not 32 bits, go fix it'
+        sys.exit(1)
     return hex(int(b_str, 2))
 
 def write_line_number(file, count):
